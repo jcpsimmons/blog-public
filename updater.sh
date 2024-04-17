b03736bb30b8fa8fdf53d50d679f37e783bc2e4d
josh simmons <joshcsimmons@hey.com>
2024-04-17 15:40:33 -0700

    test change

--- updater.sh
+++ updater.sh
@@ -4,5 +4,6 @@
 git fetch origin
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
     git pull
+    go build -o /usr/local/bin/jcsblog .
     sudo systemctl restart jcsblogd.service
 fi
