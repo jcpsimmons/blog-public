725b5f087455b28723add1fe3d8d6fe26e3b7d24
josh simmons <joshcsimmons@hey.com>
2024-04-18 09:35:53 -0700

    build script update

--- updater.sh
+++ updater.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-#  The script is pretty straightforward. It checks if there are any updates on the remote repository, pulls the updates, builds the binary, and restarts the server.
-#  The script is scheduled to run every 5 minutes using a cron job.
-#  # Path: /etc/cron.d/jcsblog
-# */5 * * * * root /usr/local/bin/updater.sh
+# Check if directory argument is provided
+if [ "$1" == "" ]; then
+    echo "Error: No directory provided. Usage: $0 <directory>"
+    exit 1
+fi
 
-#  The script also logs the update process using the  logger  command.
-#  # Path: /etc/rsyslog.d/jcsblog.conf
+# Change directory to the provided path
+cd "$1" || exit
 
+# Fetch latest changes from the origin
 git fetch origin
 
+# Check if the HEAD commit differs from the upstream
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-    # update syslog
+    # Log update
     logger -t jcsblogupdater "Updating jcsblog"
     git pull
     go build -o /usr/local/bin/jcsblog .
 
-    # check if tmux session persistent-server exists
+    # Check and terminate existing tmux session
     if tmux has-session -t persistent-server 2>/dev/null; then
         logger -t jcsblogupdater "Killing existing persistent-server tmux session"
         tmux send-keys -t persistent-server:0.0 C-c
         tmux send-keys -t persistent-server:0.0 "exit" Enter
     fi
 
+    # Start a new tmux session
     logger -t jcsblogupdater "Starting new persistent-server tmux session"
     tmux new-session -d -s persistent-server
     tmux send-keys -t persistent-server:0.0 "/usr/local/bin/jcsblog" Enter
